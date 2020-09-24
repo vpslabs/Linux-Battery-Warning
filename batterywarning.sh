1be00f2c02bee57753a8d6ddc14ef52d9a11e3c2
VPSLabs <8047727+vpslabs@users.noreply.github.com>
2020-09-24 19:42:32 +0700

    Add files via upload

---
+++ batterywarning.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+    while true
+    do
+        export DISPLAY=:0.0
+        battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
+        if on_ac_power; then
+            if [ $battery_level -ge 95 ]; then
+                notify-send "Battery charging above 95%. Unplug your AC adapter!" "Charging: ${battery_level}% "
+                if on_ac_power; then
+                    java -jar .batt_full.jar   ## Display FULLBATTERY notification window
+                fi
+             fi
+        else
+             if [ $battery_level -le 35 ]; then
+                notify-send "Battery is lower 35%. Connect your AC adapter." "Charging: ${battery_level}%"
+                if ! on_ac_power; then
+                    java -jar .batt_low.jar   ## Display LOWBATTERY notification window
+                fi
+             fi
+        fi
+
+        sleep 300 # 300 seconds or 5 minutes
+    done
